c7d6e5be6da0dcc9f608a6b04f40729a5c1d9910
naseil <nosqeil24@hanyang.ac.kr>
2016-07-11 11:16:47 +0900

    similarity module fixed.

--- similarity.py
+++ similarity.py
@@ -1,12 +1,17 @@
 import gensim
 from nltk.tokenize import word_tokenize
 def fixed_attention(sentence, question, w2v_model, embedding_converted = False):
+    ignore_word_list = ['.', ',', ':', '?']
     w_sent = word_tokenize(sentence)
     w_question = word_tokenize(question)
-    attention = 1.0
+    attention = 0.0
     for w1 in w_sent:
-        sim_list = [w2_model.similarity(w1,w2) for w2 in w_question]
+        if w1 in ignore_word_list: continue
+        sim_list = list()
+        for w2 in w_question:
+            if (w2 in ignore_word_list): continue
+            sim_list.append(abs(w2v_model.similarity(w1,w2)))
         sim_list.sort(reverse=True)
     for sim in sim_list[:3]:
-        attention = attention * sim
+        attention = attention + sim
     return attention
